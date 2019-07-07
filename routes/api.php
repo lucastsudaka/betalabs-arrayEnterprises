d00c11e031ac1897004e18495fa12ecf9ac74112
lucast <lucastsudaka@gmail.com>
2019-07-07 06:19:08 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,37 +2,47 @@
 
 use Illuminate\Http\Request;
 
-// v1 da nossa API.
+// Versão 1 da  API.
 Route::prefix('v1')->group(function(){
         /**
          * 
-         * Auth & usuario logado (current user)
+         * Auth 
          * 
          */
+        // login
         Route::post('auth/login', 'api\AuthController@login');
+        // registrar
         Route::post('auth/register', 'api\AuthController@register');
         
+        
+        /**
+         * 
+         * Current user - Usuário autenticado
+         * 
+         */
         // Ações do usuário logado (currentUser)
         Route::group(['middleware' => ['check.role:basic,admin']], function() {
+            // retornar atual usuário
             Route::get('currentUser', 'api\CurrentUserController@index');
+            // atualizar o usuário
             Route::put('currentUser', 'api\CurrentUserController@update');
+            // atualizar a foto de perfil
             Route::post('currentUser/photo', 'api\CurrentUserController@uploadPhoto');
 
-          #  Route::post('auth/currentUser/profileImage', 'api\AuthController@updateProfileImage');
 
         });
         
         
         /**
          * 
-         * Comentários
+         * Comentários -> Utilizando resource
          * 
          */   
-        Route::resource('comments','api\CommentController')->only([
+        Route::resource('comments','api\CommentController')->only([    
             'index', 'show'
         ]);
         Route::group(['middleware' => ['check.role:basic,admin']], function() {
-            Route::resource('comments','api\CommentController')->only([
+            Route::resource('comments','api\CommentController')->only([      
                 'store', 'update', 'destroy'
             ]);
         }); 
